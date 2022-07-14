a9f59933c19d889a0ebfa7e3600f1a3bf6892c47
Bui Tuan Anh <anhtb@kaiyouit.com>
2022-07-15 06:30:48 +0700

    new library

--- example/android/app/src/main/java/vn/hunghd/flutter/plugins/example/MainActivity.java
+++ example/android/app/src/main/java/vn/hunghd/flutter/plugins/example/MainActivity.java
@@ -3,11 +3,17 @@ package vn.hunghd.flutter.plugins.example;
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
+//    @Override
+//    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//        GeneratedPluginRegistrant.registerWith((PluginRegistry) flutterEngine);
+//    }
+
+//    @Override
+//    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//        GeneratedPluginRegistrant.registerWith((PluginRegistry) flutterEngine.getPlugins());
+//    }
 }
