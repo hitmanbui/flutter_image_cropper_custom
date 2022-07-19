8f300c3aaf5901a7c05f97c43ca3965e8536aef3
hitmanbui <anhtb@kaiyouit.com>
2022-07-19 10:53:49 +0700

    new library

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:io';
-
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
 
